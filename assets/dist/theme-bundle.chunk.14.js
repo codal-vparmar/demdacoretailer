66e92b8d8e3ec2465ae67f7a599fd3b999ff91b7
@vparmar1 <vparmar@codal.com>
2021-01-21 13:08:43 +0530

    pushing original code

---
+++ assets/dist/theme-bundle.chunk.14.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{70:function(e,t,c){"use strict";c.r(t),c.d(t,"default",(function(){return s}));var o=c(11),n=c(232);const a=Object(n.createCheckoutService)();class s extends o.a{async onReady(){const e=await a.loadCheckout();console.log(e.data.getCheckout())}}}}]);
+//# sourceMappingURL=theme-bundle.chunk.14.js.map
